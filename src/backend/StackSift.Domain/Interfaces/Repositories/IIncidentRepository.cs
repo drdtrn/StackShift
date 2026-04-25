1c950ecca356131d0e87d7c23c47a976a1a314d4
drdtrn <dardaniiiiiii@gmail.com>
2026-04-25 21:49:19 +0200

    Fix a bug in the handler of for GetIncidents

--- src/backend/StackSift.Domain/Interfaces/Repositories/IIncidentRepository.cs
+++ src/backend/StackSift.Domain/Interfaces/Repositories/IIncidentRepository.cs
@@ -6,5 +6,6 @@ namespace StackSift.Domain.Interfaces.Repositories;
 public interface IIncidentRepository : IRepository<Incident, Guid>
 {
     Task<IList<Incident>> GetByProjectIdAsync(Guid projectId, int page, int pageSize, IncidentStatus? status, CancellationToken ct = default);
+    Task<IList<Incident>> GetByOrganizationIdAsync(int page, int pageSize, IncidentStatus? status, CancellationToken ct = default);
     Task<int> GetOpenCountByOrganizationIdAsync(Guid organizationId, CancellationToken ct = default);
 }
